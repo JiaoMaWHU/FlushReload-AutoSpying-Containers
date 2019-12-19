569fcc3e6c5b780dbaa95ea6b05ec1e85b5185d6
JiaomaWHU <jiaomajiaoma@hotmail.com>
2019-12-19 11:16:48 -0600

    delete original runs

--- experiments/links/runs/remove_monotonicity_errors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read line; do 
-    if [[ "$line" =~ 'Monotonicity' ]] ; then 
-        echo -n $line
-    else echo $line
-fi
-done | sed 's/\[!!\] Monotonicity Error! Re-starting\.//g'
